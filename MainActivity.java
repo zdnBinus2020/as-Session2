7739541255f889704866cf57f15df994f3cb1617
zdnBinus2020 <35524521+zdnBinus2020@users.noreply.github.com>
2022-04-19 16:22:25 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.session2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText etName = findViewById(R.id.etName);
+        EditText etPassword = findViewById(R.id.etPassword);
+        TextView tvText = findViewById(R.id.textView);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+    }
+}
---
+++ MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.session2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(this,"Selamat Datang Budi", Toast.LENGTH_LONG).show();
+    }
+}
