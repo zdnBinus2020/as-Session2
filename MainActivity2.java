2bc5ff49d0f4299558c0412a9f0142166d82108f
zdnBinus2020 <35524521+zdnBinus2020@users.noreply.github.com>
2022-04-19 16:35:42 +0700

    Update MainActivity2.java

--- MainActivity2.java
+++ MainActivity2.java
@@ -11,6 +11,6 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toast.makeText(this,"Selamat Datang Budi", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Selamat Datang Budi", Toast.LENGTH_LONG).show(); // menampilkan pop up atau alert dialog sebagai info setelah sebuah aksi telah terjadi atau berhasil
     }
 }
